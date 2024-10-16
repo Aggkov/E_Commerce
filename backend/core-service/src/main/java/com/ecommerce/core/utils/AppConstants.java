013c7c9f1438201edc275e0ed9ffd2abd51f6082
Angelos Kovatsis <aggkov10@hotmail.com>
2024-10-16 18:54:49 +0300

    new gitignore

--- backend/core-service/src/main/java/com/ecommerce/core/utils/AppConstants.java
+++ backend/core-service/src/main/java/com/ecommerce/core/utils/AppConstants.java
@@ -1,13 +1,12 @@
 package com.ecommerce.core.utils;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class AppConstants {
     public static final String DEFAULT_PAGE_NUMBER = "0";
-
     public static final String DEFAULT_PAGE_SIZE = "10";
-
     public static final int MAX_PAGE_SIZE = 30;
-
     public static final String CREATED_AT = "createdAt";
-
+    public static final String UPDATED_AT = "updatedAt";
     public static final String ID = "id";
 }
